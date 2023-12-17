18ca251560295d8b55441b6942ea5902c2b76857
Connor Henn <connor.henn@yahoo.com>
2023-12-17 15:03:08 -0800

    added font variables

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+let show1 = false;
+let show2 = false;
+let show3 = false;
+let show4 = false;
+
+const listItem1 = document.getElementById('listitem1');
+const listItem2 = document.getElementById('listitem2');
+const listItem3 = document.getElementById('listitem3');
+const listItem4 = document.getElementById('listitem4');
+
